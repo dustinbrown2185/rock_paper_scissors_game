9950515183783c7523f351fb0c47cf9c9902daee
dustinbrown2185 <141280248+dustinbrown2185@users.noreply.github.com>
2023-08-15 18:40:06 -0400

    Delete enemy.py

--- enemy.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class Enemy:
-    def __init__(self, name):
-        self.name = name
-
-    def get_computer_choice(self):
-        return random.choice(["rock", "paper", "scissors"])
